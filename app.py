cd0c4d7e37a53043366fae33c393694bd668ae27
yahyatahliil-ship-it <yahya.tahliil@gmail.com>
2026-09-07 18:32:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 def get_user_data(user_input):
-  conn = sqlite3.connect("user.db")
-cursor = conn.cursor() 
-# secure: using parameterized quries prevents sql injection 
-query = "select * from users where id = ?"
-cursor.execute(query, (user_input,))
-return cursor.fetchall()
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+    # SECURE: Using parameterized queries prevents SQL injection
+    query = "SELECT * FROM users WHERE id = ?"
+    cursor.execute(query, (user_input,))
+    return cursor.fetchall()
